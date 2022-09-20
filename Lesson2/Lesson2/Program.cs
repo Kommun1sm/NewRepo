d85221dfea568d67c88fb1357d5fc3ac39421d5f
Чобанов Максим <kommun1sm@mail.ru>
2022-09-20 20:57:37 +0300

    # Класс

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        class StatisticsDigits
+        {
+            public StatisticsDigits()
+            {
+            }
+            public StatisticsDigits(int b, int c)
+            {
+            }
+            public int countNumbers(int i)
+            {
+                return (int)Math.Log10(i) + 1;
+            }
+            public int sumNumbers(int i)
+            {
+                int sum = 0;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                return sum;
+            }
+
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
